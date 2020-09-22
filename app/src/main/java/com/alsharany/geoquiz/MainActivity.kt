dd3e2e34269918b639ccb2bb500f6caada656c51
haytham taha alsharany <talsharany@gmail.com>
2020-09-22 07:09:45 +0300

    do challenge 5 chapter 3

--- app/src/main/java/com/alsharany/geoquiz/MainActivity.kt
+++ app/src/main/java/com/alsharany/geoquiz/MainActivity.kt
@@ -15,10 +15,16 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.ibb_question, true),
         Question(R.string.aden_question, false),
         Question(R.string.mareb_question, true),
-        Question(R.string.jawf_question, false),
+        Question(R.string.raima_question, true),
+        Question(R.string.hodida_question, true),
+        Question(R.string.mahra_question, true),
+        Question(R.string.lahg_question, false),
+        Question(R.string.mahweet_question, false),
 
         )
     private var currentIndex = 0
+    private var trueAnswer = 0
+    private var falseAnswer = 0
 
     private lateinit var QuestionText: TextView
     private lateinit var nextBtn: ImageButton
@@ -31,14 +37,10 @@ class MainActivity : AppCompatActivity() {
         previusBtn = findViewById(R.id.previus_btn)
         QuestionText = findViewById(R.id.Question_text)
         updateQuestion()
-
-
         true_btn.setOnClickListener {
-//            //Toast.makeText(this,R.string.Correct_toast,Toast.LENGTH_LONG).show()
-//            var  tost=  Toast.makeText(this,R.string.wrong_toast,Toast.LENGTH_LONG)
-//            tost.setGravity(Gravity.TOP,0,0)
-//            tost.show()
             checkAnswer(true)
+            if (currentIndex == QuestionBank.size - 1)
+                getGrade()
         }
         false_btn.setOnClickListener {
             // Toast.makeText(this,R.string.wrong_toast,Toast.LENGTH_LONG).show()
@@ -46,52 +48,82 @@ class MainActivity : AppCompatActivity() {
 //            tost.setGravity(Gravity.TOP,0,0)
 //            tost.show()
             checkAnswer(false)
+            if (currentIndex == QuestionBank.size - 1)
+                getGrade()
+
 
         }
         updateQuestion()
         nextBtn.setOnClickListener {
-            currentIndex = (currentIndex + 1) % QuestionBank.size
+            // currentIndex = (currentIndex + 1) % QuestionBank.size
+            checkIndex(nextBtn.id)
             updateQuestion()
-
         }
         previusBtn.setOnClickListener {
-
-            currentIndex = if (currentIndex == 0) {
-                Toast.makeText(this, "therr is no previus ", Toast.LENGTH_LONG).show()
-                currentIndex
-
-            } else
-                currentIndex - 1
+            checkIndex(previusBtn.id)
             updateQuestion()
-
         }
         QuestionText.setOnClickListener {
             currentIndex = (currentIndex + 1) % QuestionBank.size
-
             updateQuestion()
-
         }
-
     }
 
     private fun updateQuestion() {
-
+        //enable true button false button
+        btnEnableUnEbale(true)
         QuestionText.setText(QuestionBank[currentIndex].Question_text)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = QuestionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer)
+        val messageResId = if (userAnswer == correctAnswer) {
+            trueAnswer++
             R.string.Correct_toast
-        else
+        } else {
+            falseAnswer++
             R.string.wrong_toast
-       // Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show()
+        }
+        var toast = Toast.makeText(this, messageResId, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP, 0, 0)
+        toast.show()
+        btnEnableUnEbale(false)
+    }
 
-        //-------------------make toast showen in the top isead of in the bottom---------
-        var  tost=  Toast.makeText(this,messageResId,Toast.LENGTH_LONG)
-           tost.setGravity(Gravity.TOP,0,0)
-           tost.show()
+    private fun getGrade() {
+        var grade=trueAnswer * 10
+        msg_Toast("your score is : ${grade}%", Gravity.CENTER_VERTICAL)
+    }
 
+    private fun msg_Toast(msg: CharSequence, gravity: Int) {
+        // Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+        var toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.setGravity(gravity, 0, 0)
+        toast.show()
+    }
+
+    private fun checkIndex(viewId: Int) {
+        when (viewId) {
+            previusBtn.id -> {
+                currentIndex = if (currentIndex == 0) {
+                    msg_Toast("thetre is the frist Question ", Gravity.BOTTOM)
+                    currentIndex
+                } else
+                    currentIndex - 1
+            }
+            nextBtn.id -> {
+                currentIndex = if (currentIndex == QuestionBank.size - 1) {
+                    msg_Toast("there is the last Question ", Gravity.BOTTOM)
+                    currentIndex
+                } else
+                    currentIndex + 1
+            }
+        }
+    }
+
+    private fun btnEnableUnEbale(BState: Boolean) {
+        false_btn.isEnabled = BState
+        true_btn.isEnabled = BState
     }
 
 }
